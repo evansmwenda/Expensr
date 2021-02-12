9205f35b71ab54ad7f39f92d7fa79ea22eb23dfc
evansmwenda <evansmwenda.em@gmail.com>
2021-02-12 11:11:56 +0300

    added collapsing toolbar: ' :

---
+++ lib/screens/Homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  static const routeName = '/';
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Collapsing Toolbar",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
+                      )),
+                  background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("“FloatingActionButton Pressed”");
+        },
+        tooltip: "Add Expense",
+        child: new Icon(Icons.add),
+      ),
+
+
+    );
+  }
+}
