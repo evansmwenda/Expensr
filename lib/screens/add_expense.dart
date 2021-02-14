c1baf9d6809c53236da3ca73e79cddad0dd49a43
evansmwenda <evansmwenda.em@gmail.com>
2021-02-14 17:26:40 +0300

    style changes to the add expense screen

--- lib/screens/add_expense.dart
+++ lib/screens/add_expense.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:expensr/models/expense.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddExpense extends StatefulWidget {
   static const routeName = '/add-expense';
@@ -11,6 +14,22 @@ class AddExpense extends StatefulWidget {
 class _AddExpenseState extends State<AddExpense> {
   final _formKey = GlobalKey<FormState>();
   final _expense = Expense();
+  DateTime selectedDate = DateTime.now();
+  var myFormat = DateFormat('d-MM-yyyy');
+  TextEditingController _datecontroller = new TextEditingController();
+  // DateTime selectedDate =  DateFormat("dd-MM-yyyy").format(DateTime.now());
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2020, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,79 +38,88 @@ class _AddExpenseState extends State<AddExpense> {
         title: Text("Add Expense"),
       ),
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 15.0),
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
         child: Form(
-            key: _formKey,
-            child: Column(
-                children: <Widget>[
-                  // Add TextFormFields and ElevatedButton here.
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter your title of your expense.';
-                      }
-                    },
-                    onSaved: (val) => setState(() => _expense.title = val),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the amount of your expense.';
-                      }
-                    },
-                    onSaved: (val) => setState(() => _expense.amount = val),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Category'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the category of your expense.';
-                      }
-                    },
-                    onSaved: (val) => setState(() => _expense.category = val),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Date of Expense'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter the date of your expense.';
-                      }
-                    },
-                    onSaved: (val) => setState(() => _expense.date = val),
-                  ),
-                  SizedBox(height: 20,),
-                  Center(
-                    child: GestureDetector(
-                      onTap: (){
-                        final form = _formKey.currentState;
-                        if (form.validate()) {
-                          form.save();
-                          _expense.save();
-                        }
-                      },
-                      child: Container(
-                        height: 50,
-                        width: double.infinity,
-                        margin: EdgeInsets.symmetric(horizontal: 35,vertical: 15),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text("Click",
-                          style: Theme.of(context).textTheme.caption.copyWith(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ]
+          key: _formKey,
+          child: ListView(children: <Widget>[
+            // Add TextFormFields and ElevatedButton here.
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Title'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter your title of your expense.';
+                }
+              },
+              onSaved: (val) => setState(() => _expense.title = val),
             ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the amount of your expense.';
+                }
+              },
+              onSaved: (val) => setState(() => _expense.amount = val),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Category'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the category of your expense.';
+                }
+              },
+              onSaved: (val) => setState(() => _expense.category = val),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Date of Expense"),
+            InkWell(
+              onTap: () => _selectDate(context),
+              child: IgnorePointer(
+                child: TextField(
+                  controller: _datecontroller,
+                  decoration: InputDecoration(
+
+                    hintText: ('${myFormat.format(selectedDate)}'),
+                  ),
+
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: GestureDetector(
+                onTap: () {
+                  final form = _formKey.currentState;
+                  if (form.validate()) {
+                    form.save();
+                    _expense.save();
+                  }
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 35, vertical: 15),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    "Save",
+                    style: Theme.of(context).textTheme.caption.copyWith(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
